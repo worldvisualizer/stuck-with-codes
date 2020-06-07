abad0599ebab16f8339d8d3b87527b9cef13b7c2
정승우 <seung-woo@jeongseung-uui-MacBook-Pro.local>
2020-06-07 15:46:25 +0900

    working on express, and cs142 projects

--- frontend/cs142/project2/cs142-make-multi-filter.js
+++ frontend/cs142/project2/cs142-make-multi-filter.js
@@ -1,2 +1,14 @@
 'use strict';
 
+function cs142MakeMultiFilter(originalArray) {
+  return function arrayFilterer(filterCriteria, callback) {
+    let currentArray = originalArray;
+
+    if (callback) {
+      callback.bind(this);
+      callback(currentArray);
+    }
+  }
+}
+
+module.exports = cs142MakeMultiFilter;
--- frontend/cs142/project2/cs142-template-processor.js
+++ frontend/cs142/project2/cs142-template-processor.js
@@ -1,7 +1,34 @@
 'use strict';
 
-const cs142MakeMultiFilter = (originalArray) => {
-  return function(filterCriteria, callback) {
-    
+const regex = /{{\w*}}/g;
+
+class Cs142TemplateProcessor {
+
+  // template processor
+  constructor(template) {
+    this.template = template;
+  }
+
+  regexMatches() {
+    return this.template.match(regex);
+  }
+
+  fillIn(dictionary) {
+    for (let match of this.regexMatches()) {
+      let replaced = false;
+      for (let item of Object.entries(dictionary)) {
+        let [key, value] = item;
+        if (key === replaceable.substring(2, replaceable.length-2)) {
+          this.template.replace(replaceable, value);
+          replaced = true;
+        }
+      }
+
+      if (!replaced) {
+        this.template.replace(replaceable, '');
+      }
+    }
   }
 }
+
+module.exports = Cs142TemplateProcessor;
