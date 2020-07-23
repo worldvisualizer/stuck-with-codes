9dfb64aaae162746c591f78526071c1ba7d5c908
정승우 <seung-woo@homehub.olleh.com>
2020-07-23 19:22:27 +0900

    frontend/full-stack development

---
+++ javascript/vuejs/vuetify-basic-components/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-content>
+      <Playground />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Playground from '@/views/Playground'
+
+export default {
+  name: 'App',
+
+  components: {
+    Playground
+  }
+}
+</script>
---
+++ javascript/vuejs/vuetify-responsive/src/views/Cart.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col sm="4" offset-sm="2" xl="4" offset-xl="2">
+        <ProductList />
+      </v-col>
+      <v-col sm="4" xl="3">
+        <CheckoutBox />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import ProductList from '@/components/cart/ProductList.vue'
+import CheckoutBox from '@/components/cart/CheckoutBox.vue'
+
+export default {
+  components: {
+    ProductList,
+    CheckoutBox
+  }
+}
+</script>
