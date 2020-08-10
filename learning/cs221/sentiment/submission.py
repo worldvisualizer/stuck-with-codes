6d52bbdd8d7c89858817e670f4223c64eee70ac5
SeungWoo Jung <seungwoo.jung.89@gmail.com>
2020-08-10 17:35:48 +0900

    sentiment, kmeans clustering working

--- learning/cs221/sentiment/submission.py
+++ learning/cs221/sentiment/submission.py
@@ -90,6 +90,76 @@ def extractCharacterFeatures(n):
 # Problem 4: k-means
 ############################################################
 
+def euclideanDistance(loc1, loc2):
+    """
+    Return the Euclidean distance between two locations, where the locations
+    are pairs of numbers (e.g., (3, 5)).
+    """
+    def squareTwo(num1, num2):
+        return abs(num1 - num2) ** 2
+
+    distances = {}
+    for key in loc1:
+        if key in loc2:
+            distances[key] = squareTwo(loc1[key], loc2[key])
+        else:
+            distances[key] = squareTwo(loc1[key], 0)
+
+    for key in loc2:
+        if key not in distances:
+            distances[key] = squareTwo(loc2[key], 0)
+    
+    return math.sqrt(sum(list(distances.values())))
+
+
+def assignExamples(examples, assignments, centroids):
+    for index, example in enumerate(examples):
+        minLoss = 0
+        losses = sorted(
+            [
+                [centIndex, euclideanDistance(example, cent)]
+                for centIndex, cent in enumerate(centroids)
+            ],
+            key=lambda x: x[1])
+        assignments[index] = losses[0][0] # centIndex of argmin
+    return assignments
+
+
+def setCentroids(examples, assignments, centroids):
+
+    def sumDict(dict1, dict2):
+        for key in dict2:
+            dict1[key] += dict2[key]
+        return dict1
+
+    def divideDict(dictionary, count):
+        for key in dictionary:
+            dictionary[key] /= count
+        return dictionary
+
+    assert(len(examples) == len(assignments))
+    centCalculations = [[0, collections.defaultdict(float)] for i in range(len(centroids))]
+    for i, assignment in enumerate(assignments):
+        example = examples[i]
+        centCalculations[assignment] = [
+            centCalculations[assignment][0] + 1,
+            sumDict(centCalculations[assignment][1], example)
+        ]
+
+    newCentroids = []
+    for container in centCalculations:
+        count, sumofexample = container
+        newCentroids.append(divideDict(sumofexample, count))
+    return newCentroids
+
+
+def calculateTotalLoss(examples, assignments, centroids):
+    totalLoss = 0
+    for i, example in enumerate(examples):
+        cent = centroids[assignments[i]]
+        totalLoss += euclideanDistance(cent, example)
+    return totalLoss
+
 
 def kmeans(examples, K, maxIters):
     '''
@@ -100,6 +170,49 @@ def kmeans(examples, K, maxIters):
             list of assignments (i.e. if examples[i] belongs to centers[j], then assignments[i] = j)
             final reconstruction loss)
     '''
-    # BEGIN_YOUR_CODE (our solution is 25 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
-    # END_YOUR_CODE
+
+    def toDefaultDict(counterItem):
+        defaultItem = collections.defaultdict(float)
+        for key in counterItem.keys():
+            defaultItem[key] = counterItem[key]
+        return defaultItem
+
+    def initializeCentroid(items):
+        modelExample = collections.defaultdict(list)
+        for item in items:
+            for key, value in item.items():
+                if modelExample[key]:
+                    curmin, curmax = modelExample[key]
+                    if curmin != None:
+                        if value < curmin:
+                            curmin = value
+                    if curmin != None:
+                        if value > curmax:
+                            curmax = value
+                    modelExample[key] = [curmin, curmax]
+                else:
+                    modelExample[key] = [0.0, 0.0]
+
+        return modelExample
+
+    def randomCentroid(modelExample):
+        randomExample = collections.defaultdict(float)
+        for key, value in modelExample.items():
+            minimum, maximum = value
+            randomExample[key] = random.uniform(minimum, maximum)
+        return randomExample
+
+    # assignment after creating a copy of the original works
+    examples = [toDefaultDict(item) for item in examples]
+    # create assignment index for each of the examples
+    assignments = [0 for i in range(len(examples))]
+    modelExample = initializeCentroid(examples)
+    centroids = [randomCentroid(modelExample) for i in range(K)]
+
+    for i in range(maxIters):
+        assignments = assignExamples(examples, assignments, centroids)
+        centroids = setCentroids(examples, assignments, centroids)
+
+    reconstructionLoss = calculateTotalLoss(examples, assignments, centroids)
+
+    return (centroids, assignments, reconstructionLoss)
